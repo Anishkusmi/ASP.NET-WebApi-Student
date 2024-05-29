42b4a843f9ed5415cc6d26668a522cf87a3a46f5
Anish Kumar Chaudhary <134726737+Anishkusmi@users.noreply.github.com>
2024-05-29 10:55:29 +0545

    API call for Employee

---
+++ EmployeeDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Aps.NetFirstWebApi.Controllers
+{
+    public class EmployeeDataController : ApiController
+    {
+        //  By Default Asp.Net Web Api return XML formate data
+        string[] myEmployee = { "ani", "Anish", "Ram" };
+        [HttpGet]  //By default
+        public string[] GetEmployee() 
+        {
+            //return new string[] {}
+            return myEmployee;
+        }
+        public string GetEmployeeByIndex(int id)
+        {
+            return myEmployee[id];
+        }
+    }
+}
